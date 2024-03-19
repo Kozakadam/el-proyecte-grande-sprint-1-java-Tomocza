71c95174216811437d61b3f8bb4bbf633fe147d7
Kozakadam <kar.adam.robert@gmail.com>
2024-03-19 12:52:39 +0100

    change export method

--- frontend/src/providers/quizProvider.js
+++ frontend/src/providers/quizProvider.js
@@ -1,4 +1,4 @@
-async function updateQuizName(quizName, quizId) {
+export async function updateQuizName(quizName, quizId) {
   const httpRes = await fetch(`/api/quiz/${quizId}`, {
     method: "PATCH",
     body: JSON.stringify({
@@ -12,43 +12,43 @@ async function updateQuizName(quizName, quizId) {
   return await httpRes.json();
 }
 
-async function fetchQuizById(quizId) {
+export async function fetchQuizById(quizId) {
   const httpRes = await fetch(`/api/quiz/${quizId}`);
   return await httpRes.json();
 }
 
-async function fetchAllQuizzes() {
+export async function fetchAllQuizzes() {
   const httpRawRes = await fetch("/api/quiz/public");
   return await httpRawRes.json();
 }
 
-async function fetchMyQuizzes() {
+export async function fetchMyQuizzes() {
   const httpRawRes = await fetch("/api/quiz/own");
   return await httpRawRes.json();
 }
 
-async function saveEmptyQuiz() {
+export async function saveEmptyQuiz() {
   const httpRes = await fetch(`/api/quiz/create`, {
     method: "POST"
   });
   return await httpRes.json();
 }
 
-async function copyQuiz(quizId) {
+export async function copyQuiz(quizId) {
   const httpRes = await fetch(`/api/quiz/copy/${quizId}`, {
     method: "POST"
   });
   return await httpRes.json();
 }
 
-async function deleteQuizById(quizId) {
+export async function deleteQuizById(quizId) {
   const httpRes = await fetch(`/api/quiz/${quizId}`, {
     method: "DELETE"
   });
   return await httpRes.json();
 }
 
-async function fetchModifiedAtById(quizId) {
+export async function fetchModifiedAtById(quizId) {
   const httpRes = await fetch(`/api/quiz/modified/${quizId}`);
   return await httpRes.json();
 }
